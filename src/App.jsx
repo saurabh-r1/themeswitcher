efbdcc6f29791b632e693e6e73835387351ee380
saurabh-r1 <sidlock2@gmail.com>
2023-09-21 05:16:09 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-pink-600 p-4 text-6xl'>Hello</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
